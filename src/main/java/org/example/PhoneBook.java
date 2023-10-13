9ca051ab3c3496707e61a3c99766054d70ea23f9
alexfateev <mailboxfav@gmail.com>
2023-10-13 12:03:02 +0500

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -9,6 +9,7 @@ public class PhoneBook {
     private Map<String, String> contacts = new HashMap<>();
 
     public int add(String name, String number) {
-        return 0;
+        contacts.putIfAbsent(name, number);
+        return contacts.size();
     }
 }
